32004fedee1f65a29f4e3801e1631066d04ce3d1
chromableedstudios <chromableedstudios@c141159e-bf63-11de-a8ef-132414f8baa6>
2012-08-12 03:58:15 +0000

    +add nv class

---
+++ trunk/cdmaDevLib/cdmaDevLib/qcdm/Nv.vb
@@ -0,0 +1,9 @@
+﻿Public Class Nv
+    Property Data As String
+    Property Item As String
+
+    Sub New(_Item As NvItems.NVItems, _Data As String)
+        Data = _Data
+        Item = _Item.ToString
+    End Sub
+End Class
